3584d9c5d533a44cbe0d2348f68a1bdaf2a20faa
kpilszak <klaudia.pilszak@gmail.com>
2022-02-21 16:16:07 +0100

    Create repo

---
+++ src/main/java/com/kpilszak/microbank/MicroBankApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.microbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBankApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBankApplication.class, args);
+	}
+	
+}
